4e0f3da0737ec29906eb439ac4a1d056efba9d1a
Maizi Liao <Wheat@Maizis-MacBook-Pro.local>
2017-02-01 18:47:02 -0500

    initial commit

---
+++ CurrencyConverter.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+"""Simple Currency Comverter"""
+
+from __future__ import print_function
+
+from urllib2 import urlopen, URLError
+from HTMLParser import HTMLParser
+from urllib import urlopen
+from sys import argv
+
+FROM = "https://www.google.com/finance/converter?a=1&from="
+TO = "&to="
+CONNECTION_MSG = "Cannot connect to Google Currency Converter!"
+INPUT_MSG ="""Invalid arguments: {}\n---------
+Examples:
+		$ CurrencyConverter
+		$ CurrencyConverter CAD CNY
+		$ CurrencyConverter 10 USD CNY
+"""
+
+class CurrencyParser(HTMLParser):
+	# Extract data (including nested data) from tag with id = currency_converter_result
+	def __init__(self):
+		HTMLParser.__init__(self)
+		self.recording = 0
+		self.data = []
+
+	def handle_starttag(self, tag, attributes):
+		if tag != 'div':
+			return
+		if self.recording:
+			self.recording += 1
+			return
+		for name, value in attributes:
+			if name == 'id' and value == 'currency_converter_result':
+				break
+		# Loop statements may have an else clause;
+		# it is executed when the loop terminates through
+		# exhaustion of the list (with for) or 
+		# when the condition becomes false (with while)
+		# So 'break' will ignore the else clause
+		else:
+			return
+		self.recording = 1
+
+	def handle_endtag(self, tag):
+		if tag == 'div' and self.recording:
+			self.recording -= 1
+
+	def handle_data(self, data):
+		if self.recording:
+			self.data.append(data)
+
+# Get HTML from Google Currency Converter
+def getResult(url):
+	try:
+		response = urlopen(url)
+	except (URLError, IOError) as e:
+		print(CONNECTION_MSG)
+		return
+	result = response.read()
+	response.close()
+	return result
+
+def printResult(url):
+	result = getResult(url)
+	p = CurrencyParser()
+	p.feed(result)
+	for i in p.data:
+		print(i, end="")
+
+def main(args = argv[1:]):
+	# 0 argument
+	if len(args) == 0:
+		url = FROM + "CAD" + TO + "CNY"
+		printResult(url)
+		url = FROM + "USD" + TO + "CNY"
+		printResult(url)
+	# 2 arguments
+	elif len(args) == 2:
+		url = FROM + args[0] + TO + args[1]
+		printResult(url)
+	# 3 arguments
+	elif len(args) == 3 and args[0].isdigit():
+		url = FROM.replace("1", args[0]) + args[1] + TO + args[2]
+		printResult(url)
+	# invalid arguments
+	else:
+		print(INPUT_MSG)
+
+if __name__ == '__main__':
+	main()
---
+++ XiaChe.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Simple Zhihu XiaChe Reader"""
+
+from urllib2 import urlopen, URLError
+from HTMLParser import HTMLParser
+from urllib import urlopen
+from json import loads
+
+CONNECTION_MSG = "Cannot connect to Zhihu!"
+LIST_URL = "http://news-at.zhihu.com/api/3/news/latest"
+CONTENT_URL = "http://news-at.zhihu.com/api/4/news/"
+
+class XiaCheParser(HTMLParser):
+	# Extract data from tag <h2> and <p>
+	# Insert a newline before <h2> for easy reading
+	def __init__(self):
+		HTMLParser.__init__(self)
+		self.recording = 0
+		self.data = []
+
+	def handle_starttag(self, tag, attributes):
+		if tag == u'h2':
+			self.recording = 1
+		elif tag == u'p':
+			self.recording = 2
+
+	def handle_endtag(self, tag):
+		self.recording = 0
+
+	def handle_data(self, data):
+		if self.recording == 1:
+			self.data.append('\n')
+			self.data.append(data)
+		elif self.recording == 2:
+			self.data.append(data)
+
+# Get JSON from ZhiHu API
+def getResult(url):
+	try:
+		response = urlopen(url)
+	except (URLError, IOError) as e:
+		print(CONNECTION_MSG)
+		return
+	result = response.read()
+	response.close()
+	return result
+
+# Parse JSON to get proper ID for XiaChe
+def getID(url):
+	parsedJSOM = loads(getResult(url))
+	for i in parsedJSOM["stories"]:
+		# \u778e\u626f is unicode of XiaChe
+		if u'\u778e\u626f' in i["title"]:
+			return str(i["id"])
+
+def printResult(url):
+	parsedJSOM = loads(getResult(url))
+	html = parsedJSOM["body"]
+	p = XiaCheParser()
+	p.feed(html)
+	p.data.append('\n')
+	for i in p.data:
+		print(unicode(i).encode('utf8'))
+
+def main():
+	printResult(CONTENT_URL + getID(LIST_URL))
+
+if __name__ == '__main__':
+	main()
